c9da1ca0a4e71bd83b228364e5cb4cafc5ed9229
Gabriel Galvão dos Anjos <136500240+gabrielgalva@users.noreply.github.com>
2023-07-05 15:43:05 -0300

    Update Exercício Seção 3.sql

--- Sprint 2/Exercício Seção 3.sql
+++ Sprint 2/Exercício Seção 3.sql
@@ -1,26 +1,26 @@
-<h1>Execício - AWS 2/10</h1>
+Execício - AWS 2/10
 
-<h2>• Linguagem SQL</h2>
-<h4>-  Abaixo compartilhei os exercícios resolvidos na seção 3 sobre SQL. </h4>
+• Linguagem SQL
+-  Abaixo compartilhei os exercícios resolvidos na seção 3 sobre SQL. 
 
-<h2>• Caso de estudo"Biblioteca"</h2>
+• Caso de estudo"Biblioteca"
   
 
-<h3>- Exercício de programação 1: E1</h3>
+- Exercício de programação 1: E1
 select DISTINCT *
 from livro 
 WHERE livro.publicacao > '2015-01-01'
 ORDER BY livro.cod  ASC;
 
 
-<h3>- Exercício de programação 2: E2</h3>
+- Exercício de programação 2: E2
 select titulo, valor 
 from livro l 
 ORDER by valor desc
 limit 10 
 
   
-<h3>- Exercício de programação 3: E3</h3>
+- Exercício de programação 3: E3
 SELECT COUNT(livro.cod) AS quantidade, editora.nome, endereco.estado, endereco.cidade
 FROM livro
 INNER JOIN editora ON livro.editora = editora.codeditora
@@ -30,7 +30,7 @@ ORDER BY quantidade DESC
 LIMIT 5;
 
 
-<h3>- Exercício de programação 4: E4</h3>
+- Exercício de programação 4: E4
 SELECT autor.codautor, autor.nome, autor.nascimento, COUNT(livro.cod) AS quantidade
 FROM autor
 LEFT JOIN livro ON livro.autor = autor.codautor
@@ -38,7 +38,7 @@ GROUP BY autor.codautor, autor.nome, autor.nascimento
 ORDER BY autor.nome ASC;
 
 
-<h3>- Exercício de programação 5: E5</h3>
+- Exercício de programação 5: E5
 SELECT DISTINCT 
     a.nome
 FROM 
@@ -53,7 +53,7 @@ WHERE UPPER(en.estado) NOT IN ('PARANÁ', 'RIO GRANDE DO SUL', 'SANTA CATARINA')
 ORDER BY a.nome ASC;
 
 
-<h3>- Exercício de programação 6: E6</h3>
+- Exercício de programação 6: E6
 SELECT a.codautor, a.nome, COUNT(l.cod) AS quantidade_publicacoes
 FROM autor a
 INNER JOIN livro l ON a.codautor = l.autor
@@ -66,8 +66,7 @@ HAVING COUNT(l.cod) = (
     LIMIT 1
 );
 
-
-<h3>- Exercício de programação 7: E7</h3>
+- Exercício de programação 7: E7
 select
     nome
 from
