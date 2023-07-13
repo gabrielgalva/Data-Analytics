8ae2391171467fff4ee72062ffe7db61cc6f877a
Gabriel Galvão dos Anjos <gabrieldosanjos136@gmail.com>
2023-07-13 16:23:44 -0300

    Update

--- sprint3/exercicio/secao4/ex10.py
+++ sprint3/exercicio/secao4/ex10.py
@@ -3,7 +3,7 @@
 def remover_duplicatas(lista):
     return list(set(lista))
 
-original = ['abc', 'abc', 'abc', '123', 'abc', '123', '123']
-lista_sem_duplicatas = remover_duplicatas(original)
+Original = ['abc', 'abc', 'abc', '123', 'abc', '123', '123']
+lista_sem_duplicatas = remover_duplicatas(Original)
 
 print(lista_sem_duplicatas)
