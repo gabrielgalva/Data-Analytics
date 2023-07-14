8864034fdbf4006a140fa74cea0e3a05c0ed408e
Gabriel Galvão dos Anjos <136500240+gabrielgalva@users.noreply.github.com>
2023-07-14 15:50:31 -0300

    Update

--- sprint3/exercicio/secao4/ex08.py
+++ sprint3/exercicio/secao4/ex08.py
@@ -6,4 +6,4 @@ for palavra in palavras:
     if palavra == palavra[::-1]:
         print("A palavra:", palavra, "é um palíndromo")
     else:
-        print("A palavra:", palavra, "não é um palíndromo"
+        print("A palavra:", palavra, "não é um palíndromo")
