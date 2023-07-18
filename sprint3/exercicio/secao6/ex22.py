c3931806e89ddf5f02ebfd648eda54c045afd633
Gabriel Galvão dos Anjos <gabrieldosanjos136@gmail.com>
2023-07-18 15:38:21 -0300

    Update

--- sprint3/exercicio/secao6/ex22.py
+++ sprint3/exercicio/secao6/ex22.py
@@ -1,1 +1,16 @@
 # Exercício de programação 22: Exercícios Parte 1
+
+class Pessoa:
+    def __init__(self, identificador):
+        self.id = identificador
+        self.__nome = ""
+
+    def nome(self):
+        return self.__nome
+
+    def nome(self, valor):
+        self.__nome = valor
+
+pessoa = Pessoa(0)
+pessoa.nome = 'Fulano De Tal'
+print(pessoa.nome)
