581d727b256cd8f9ac0897d66af9f89d69282d09
Gabriel Galvão dos Anjos <gabrieldosanjos136@gmail.com>
2023-07-19 14:07:01 -0300

    Update

--- sprint3/exercicio/secao4/ex18.py
+++ sprint3/exercicio/secao4/ex18.py
@@ -1,4 +1,5 @@
 # Exercício de programação 18: Exercícios Parte 2
+
 speed = {'jan': 47, 'feb': 52, 'march': 47, 'April': 44, 'May': 52, 'June': 53, 'july': 54, 'Aug': 44, 'Sept': 54}
 
 valores= set(speed.values())
