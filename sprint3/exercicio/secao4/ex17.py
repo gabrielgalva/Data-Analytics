073686d8869a93d6ffa5a4245310ae010cfac13c
Gabriel Galvão dos Anjos <gabrieldosanjos136@gmail.com>
2023-07-24 09:17:58 -0300

    Update

--- sprint3/exercicio/secao4/ex17.py
+++ sprint3/exercicio/secao4/ex17.py
@@ -1,12 +1,13 @@
 # Exercício de programação 17: Exercícios Parte 2
 
 def dividir_lista(lista):
-  tamanho = len(lista)
-  tamanho_parte = tamanho // 3
-  parte1 = lista[:tamanho_parte]
-  parte2 = lista[tamanho_parte: tamanho_parte * 2]
-  parte3 = lista[tamanho_parte * 2:]
-  return parte1,parte2,parte3
+
+tamanho = len(lista)
+tamanho_parte = tamanho // 3
+parte1 = lista[:tamanho_parte]
+parte2 = lista[tamanho_parte: tamanho_parte * 2]
+parte3 = lista[tamanho_parte * 2:]
+return parte1,parte2,parte3
 
 lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 parte1, parte2, parte3 = dividir_lista(lista)
